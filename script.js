9e322b97b805e5e014abf53a1b68b059d7e7c057
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-21 12:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function makeid(l) {
   // write your code here
+	// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	let str="";
+	for(let i=0;i<l;i++){
+		
+		str+= String.fromCharCode(generateRandomNum());
+	}
+	return str;
+	
 }
+function generateRandomNum() {
+	const randomIndex =parseInt(Math.random() * 100);
+		if(randomIndex<47){
+			randomIndex+=47;
+			return randomIndex;
+		}
+}
+
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
